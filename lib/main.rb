f0f20765b64e5783347f6b1a7888e5786ffd834d
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2021-02-08 17:34:44 -0700

    add empty main node.rb

---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+#require 'pry'; binding.pry #rubocop:disable all
+
+require_relative 'node'
+
+puts 'asdf'
---
+++ lib/node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+#require 'pry'; binding.pry #rubocop:disable all
+
+require_relative 'node'
+
+# node within a binary search tree
+class Node
+  def initialize
+    # asdf
+  end
+end
